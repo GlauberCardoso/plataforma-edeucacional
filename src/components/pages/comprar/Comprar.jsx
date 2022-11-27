02ad4333470e435cc1a7d47d244604fd2db833d1
Jessany Kaline <jessany42@gmail.com>
2022-11-27 13:38:40 -0300

    modifiquei a page login

--- src/components/pages/comprar/Comprar.jsx
+++ src/components/pages/comprar/Comprar.jsx
@@ -1,7 +1,160 @@
-import React from 'react'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import StarIcon from '@mui/icons-material/StarBorder';
+import Typography from '@mui/material/Typography';
+import GlobalStyles from '@mui/material/GlobalStyles';
+import Container from '@mui/material/Container';
 
-export default function Comprar() {
+import Menu from "../../molecules/Menu";
+
+
+
+
+
+const tiers = [
+  {
+    title: 'Básico',
+    price: '70',
+    description: [
+      'Vídeo-aulas',
+      'Aulas ao vivo',
+      'Professores especializados',
+      'Suporte de experts',
+    ],
+    buttonText: 'Comprar',
+    buttonVariant: 'outlined',
+  },
+  {
+    title: 'Avançado',
+    subheader: 'Plano mais popular',
+    price: '115',
+    description: [
+      
+      'Vídeo-aulas',
+      'Aulas ao vivo',
+      'Professores especializados',
+      'Acesso a todos os planos',
+      'Prioridade no suporte de experts',
+    ],
+    buttonText: 'Comece Agora',
+    buttonVariant: 'contained',
+  },
+  {
+    title: 'Intermediário',
+    price: '95',
+    description: [
+      'Vídeo-aulas',
+      'Aulas ao vivo',
+      'Professores especializados',
+      'Acesso ao plano básico',
+      'Suporte de experts',
+    ],
+    buttonText: 'Comprar',
+    buttonVariant: 'outlined',
+  },
+];
+
+
+
+function PricingContent() {
   return (
-    <div>Comprar</div>
-  )
+    <React.Fragment>
+      <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
+      <CssBaseline />
+      <Menu />
+      
+      {/* Hero unit */}
+      <Container disableGutters maxWidth="sm" component="main" sx={{ pt: 8, pb: 3, marginTop: 8 }}>
+        <Typography
+          component="h3"
+          variant="h3"
+          align="center"
+          color="text.primary"
+          gutterBottom
+        >
+          Start Now!
+        </Typography>
+        
+      </Container>
+      {/* End hero unit */}
+      <Container maxWidth="md" component="main">
+        <Grid container spacing={5} alignItems="flex-end">
+          {tiers.map((tier) => (
+            // Enterprise card is full width at sm breakpoint
+            <Grid
+              item
+              key={tier.title}
+              xs={12}
+              sm={tier.title === 'Enterprise' ? 12 : 6}
+              md={4}
+            >
+              <Card>
+                <CardHeader
+                  title={tier.title}
+                  subheader={tier.subheader}
+                  titleTypographyProps={{ align: 'center' }}
+                  action={tier.title === 'Pro' ? <StarIcon /> : null}
+                  subheaderTypographyProps={{
+                    align: 'center',
+                  }}
+                  sx={{
+                    backgroundColor: (theme) =>
+                      theme.palette.mode === 'light'
+                        ? theme.palette.grey[200]
+                        : theme.palette.grey[700],
+                  }}
+                />
+                <CardContent>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'baseline',
+                      mb: 2,
+                    }}
+                  >
+                    <Typography component="h3" variant="h3" color="text.primary">
+                      R${tier.price}
+                    </Typography>
+                    <Typography variant="h6" color="text.secondary">
+                      /mês
+                    </Typography>
+                  </Box>
+                  <ul>
+                    {tier.description.map((line) => (
+                      <Typography
+                        component="li"
+                        variant="subtitle1"
+                        align="center"
+                        key={line}
+                      >
+                        {line}
+                      </Typography>
+                    ))}
+                  </ul>
+                </CardContent>
+                <CardActions>
+                  <Button fullWidth variant={tier.buttonVariant}>
+                    {tier.buttonText}
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+      
+    </React.Fragment>
+  );
+}
+
+export default function Pricing() {
+  return <PricingContent />;
 }
--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -1,40 +1,116 @@
-import React from "react";
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import background from "./style/backgroundmedium.jpg";
 import Menu from "../../molecules/Menu";
 
 
-export default function Login() {
-  return (
-    <>
-    <Menu />
-    <div id="login-container">
-      <form action="" method="">
-        <label htmlFor="email"></label>
-        <input
-          type="email"
-          name="email"
-          autocomplete="email"
-          placeholder="E-mail"
-        />
-        <label htmlFor="password"></label>
-        <input
-          type="password"
-          name="password"
-          minlength="8"
-          title="Coloque no mínimo 8 caracteres"
-          placeholder="Senha"
-          autocomplete="on"
-        />
-        <a href="#" id="esqueceu-pass">
-          Esqueceu sua senha?
-        </a>
-        <input type="submit" value="Login" />
-      </form>
 
-      <div id="cadastro-container">
-        <p>OU</p>
-        <a href="#">Cadastre-se</a>
-      </div>
-    </div>
-    </>
+const theme = createTheme();
+
+export default function SignInSide() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Menu />
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: `url(${background})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 15,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            
+            <Typography component="h3" variant="h4" sx={{marginTop: 8}}>
+              Login
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 5 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="E-mail"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Senha"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Salvar"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Entrar
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Esqueceu a senha?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Não tem conta? Cadastra-se"}
+                  </Link>
+                </Grid>
+              </Grid>
+              
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 }
