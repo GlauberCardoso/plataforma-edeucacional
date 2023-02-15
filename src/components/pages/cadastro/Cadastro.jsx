65af29b445832e72a857bbfac5b924d9abc00f31
GlauberCardoso <glauberdevs@gmail.com>
2023-02-14 21:26:18 -0300

    Melhorei a Responsividade da Home Page

--- src/components/pages/cadastro/Cadastro.jsx
+++ src/components/pages/cadastro/Cadastro.jsx
@@ -1,4 +1,4 @@
-/*import React from "react";
+import React from "react";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
@@ -300,4 +300,3 @@ export default function SignUp() {
     </>
   );
 }
-*/
